d92d6835e09e96844f6536861381d40073f3b662
Bruno Canini <brunocanini1994@gmail.com>
2023-04-14 22:05:56 +0200

    scheletro base bis

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-alert("BENVENUTO IN TRENI-BOOLEAN")
+const kilometri = document.getElementById("kmUtente");
 
-const kmUtente = parseInt( prompt("Km che si vogliono percorrere?"))
+const etaUtente = document.getElementById("anniUtente");
+
+let sconto = 0
+
+let prezzoBiglietto = 0
+
+const button = document.getElementById("bottone");
+button.addEventListener("click", function(){
+    const kilometri = document.getElementById("kmUtente").value;
+    const etaUtente = document.getElementById("anniUtente").value;
+
+    const prezzoKm = kilometri*0.21;
+
+    if(etaUtente == "minorenne"){
+        sconto = 20;
+    } else if (etaUtente == "over65"){
+        sconto = 40;
+    } else if (etaUtente == "maggiorenne"){
+        sconto = 0;
+    }
+
+    prezzoBiglietto = prezzoKm - (prezzoKm * sconto /100);
+    prezzoBiglietto = Math.floor(prezzoBiglietto * 100) / 100;
+
+    console.log(kilometri);
+    console.log(etaUtente);
+    console.log(prezzoKm);
+    console.log(sconto);
+    console.log(prezzoBiglietto);
+});
